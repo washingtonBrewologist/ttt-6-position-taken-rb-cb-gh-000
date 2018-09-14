19282f13c7afdb067bd91adf3662fa7d3daf3c4f
Nick Williams <nwilliams7539@student.whatcom.edu>
2018-09-14 19:43:26 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,9 +1,11 @@
 # code your #position_taken? method here!
+player_char = "X"
+comp_char = "O"
 
 def position_taken?(board, index)
 if board[index] = " "
   return false
-elsif board[index] != "X" || "O"
+elsif board[index] = player_char || board[index] = comp_char
   return true
  end
 end
