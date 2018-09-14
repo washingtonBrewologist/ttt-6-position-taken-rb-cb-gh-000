b53f3afa786330a283964e3243eb576e11d7ef6e
Nick Williams <nwilliams7539@student.whatcom.edu>
2018-09-14 19:48:53 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,7 +2,7 @@
 def position_taken?(board, index)
 if board[index] == " " || "" || nil
   return false
-elsif board[index] == "X"
+elsif board[index] == "X" || board[index] == "O"
   return true
  end
 end
